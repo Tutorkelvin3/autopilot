408ac84df1cf664d663ea4c6f66f3eb08e4e9d37
Chinaecherem <farmercistfirstson@gmail.com>
2026-05-13 03:51:14 +0100

    fix: TradeLog import case

--- Frontend/src/pages/AgentDashboard.jsx
+++ Frontend/src/pages/AgentDashboard.jsx
@@ -8,7 +8,7 @@ import { updateOnChain } from '../agent/Executor';
 import { getStats, formatTime } from '../agent/Memory';
 import { STRATEGY_META, codeToStrategy } from '../agent/Brain';
 import StatusBadge from '../components/StatusBadge';
-import TradeLog from '../components/Tradelog';
+import TradeLog from '../components/TradeLog';
 import PerformanceChart from '../components/Performancechart';
 
 const AUTO_CYCLE_MS = 5 * 60 * 1000;
