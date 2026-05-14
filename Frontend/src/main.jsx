d4cf6d95248a1e2467c0c279a6b20b81c4e670a9
Chinaecherem <farmercistfirstson@gmail.com>
2026-05-14 08:29:48 +0100

    Disable wallet autoConnect

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -28,7 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider networks={networks} defaultNetwork="testnet">
-        <WalletProvider autoConnect>
+        <WalletProvider autoConnect={false}>
           <BrowserRouter>
             <App />
           </BrowserRouter>
